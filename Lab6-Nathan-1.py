61394204deee79a8c4681a14095b2ef4e7311c02
Nathan Brooks <nathangamer0806@gmail.com>
2026-02-17 17:30:11 -0500

    setting all dictionaries up

---
+++ Lab6-Nathan-1.py
@@ -0,0 +1,11 @@
+"""
+Nathan Brooks
+2/17/2025
+User Login System
+"""
+
+data = {"Nbrooks": "verysecurepass",
+        "guest": "guest",
+        "Mars": "starrynight",
+        "Neptune": "blueorbital",
+        }
