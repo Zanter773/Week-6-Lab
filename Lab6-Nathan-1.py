bd73459d734bc37b9a3bc0dffd6b1d2e2a6b2fba
Nathan Brooks <nathangamer0806@gmail.com>
2026-02-17 17:48:04 -0500

    3 strike system for passwords

--- Lab6-Nathan-1.py
+++ Lab6-Nathan-1.py
@@ -4,7 +4,7 @@ Nathan Brooks
 User Login System
 """
 
-data = {"Nbrooks": "verysecurepass",
+userInfo = {"Nbrooks": "verysecurepass",
         "guest": "guest",
         "Mars": "starrynight",
         "Neptune": "blueorbital",
@@ -14,15 +14,18 @@ username = input("What is your username? \n")
 attempts = 3
 
 
-if username in data:
-    password = input("What is your password? \n")
-    if password == data[username]:
-        print(f'Welcome, {username}.')
-        if username == 'guest':
-            print("You have Guest access.")
+if username in userInfo:
+    while attempts > 0:
+        password = input("What is your password? \n")
+        if password == userInfo[username]:
+            print(f'Welcome, {username}.')
+            if username == 'guest':
+                print("You have Guest access.")
+            else:
+                print("You have Security Level 1.")
         else:
-            print("You have Security Level 1.")
-    else:
-        print("Incorrect Password.")
+            print("Incorrect Password.")
+            attempts -= 1
+    print("You have been locked out.")
 else:
     print("User not found. Exiting.")
