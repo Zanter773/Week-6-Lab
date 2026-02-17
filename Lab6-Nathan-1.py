0bb2b390be8d71ca6eec041db6a9990febf27932
Nathan Brooks <nathangamer0806@gmail.com>
2026-02-17 17:45:12 -0500

    fixed bug with password accepting nearly anything

--- Lab6-Nathan-1.py
+++ Lab6-Nathan-1.py
@@ -11,16 +11,18 @@ data = {"Nbrooks": "verysecurepass",
         }
 
 username = input("What is your username? \n")
+attempts = 3
+
 
 if username in data:
     password = input("What is your password? \n")
-    if password in data[username]:
+    if password == data[username]:
         print(f'Welcome, {username}.')
         if username == 'guest':
             print("You have Guest access.")
         else:
             print("You have Security Level 1.")
     else:
-        print("Incorrect Password. Exiting.")
+        print("Incorrect Password.")
 else:
     print("User not found. Exiting.")
